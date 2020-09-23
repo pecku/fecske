ea6129eba924df83ade96852a959c6b2b8f8322c
pecku <65392138+pecku@users.noreply.github.com>
2020-09-23 13:48:05 +0200

    Create ChatMode.js

---
+++ ChatMode.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         ChatMode
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.facebook.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    /*
+    var contactsElement = document.querySelector('[data-pagelet="RightRail"]').firstElementChild.nextElementSibling;
+    var buttonParent = contactsElement.querySelector('[aria-label="New room"]').parentElement.parentElement.parentElement;
+    //var clonedButton = buttonParent.firstElementChild.cloneNode(true);
+
+    var notReallyClonedButton = document.createElement('input');
+    notReallyClonedButton.type="button";
+    notReallyClonedButton.id="nrcb";
+    notReallyClonedButton.value="CM";
+
+    buttonParent.insertBefore(notReallyClonedButton, buttonParent.firstElementChild);
+    */
+
+    var elementsWithNavRole = document.querySelectorAll('[role="navigation"]');
+    var arrayNeedToBeHidden = Array.from(elementsWithNavRole);
+    arrayNeedToBeHidden.push(document.querySelector('[role="banner"]'));
+    //arrayNeedToBeHidden.push(document.querySelector('[role="main"]'));
+    arrayNeedToBeHidden.push(document.querySelector('[data-pagelet="RightRail"]').firstElementChild);
+
+    var contactsElement = document.querySelector('[data-pagelet="RightRail"]').firstElementChild.nextElementSibling.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild;
+
+    var chatMode = false;
+    contactsElement.addEventListener('click',
+                                     function(){
+        if(chatMode){
+            arrayNeedToBeHidden.map(f => (f.hidden=false));
+            chatMode=false;
+            document.querySelector('[role="main"]').style.display=""
+        }else{
+            arrayNeedToBeHidden.map(f => (f.hidden=true));
+            document.querySelector('[role="main"]').style.display="none";
+            chatMode=true;}})
+
+    //arrayOfElementsWithNavRole.map(f => (f.style.display = "none"));
+
+
+
+})();
