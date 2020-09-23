e6d37421748ccbff4657f89b15e57236ad6452a3
pecku <65392138+pecku@users.noreply.github.com>
2020-09-23 18:43:42 +0200

    Update ChatMode.js

--- ChatMode.js
+++ ChatMode.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatMode
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.facebook.com/
@@ -11,25 +11,26 @@
 (function() {
     'use strict';
     
-    var elementsWithNavRole = document.querySelectorAll('[role="navigation"]');
+    var elementsWithNavRole = document.querySelectorAll('[role="navigation"], [role="banner"]');
     var arrayNeedToBeHidden = Array.from(elementsWithNavRole);
-    arrayNeedToBeHidden.push(document.querySelector('[role="banner"]'));
     arrayNeedToBeHidden.push(document.querySelector('[data-pagelet="RightRail"]').firstElementChild);
 
     var contactsElement = document.querySelector('[data-pagelet="RightRail"]').firstElementChild.nextElementSibling.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild;
+    var mainElement = document.querySelector('[role="main"]');
 
     var chatMode = false;
     contactsElement.addEventListener('click',
                                      function(){
         if(chatMode){
             arrayNeedToBeHidden.map(f => (f.hidden=false));
+            mainElement.style.display="";
             chatMode=false;
-            document.querySelector('[role="main"]').style.display=""
         }else{
             arrayNeedToBeHidden.map(f => (f.hidden=true));
-            document.querySelector('[role="main"]').style.display="none";
-            chatMode=true;}
-        })
+            mainElement.style.display="none";
+            chatMode=true;
+        }
+    })
     
     var modifiedChatWindow = document.createElement('style');
     modifiedChatWindow.type = "text/css";
